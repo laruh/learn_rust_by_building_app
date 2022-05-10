f01ed3c25ff76ca1686225f8172120e533326a73
laruh <oblomoff616@gmail.com>
2022-05-10 21:55:01 +0700

    Topic: Generics & Structures

---
+++ src/bin/generics_structures.rs
@@ -0,0 +1,55 @@
+// Topic: Generics & Structures
+//
+// Requirements:
+// * Create a Vehicle structure that is generic over traits Body and Color
+// * Create structures for vehicle bodies and vehicle colors and implement the
+//   Body and Color traits for these structures
+// * Implement a 'new' function for Vehicle that allows it to have any body
+//   and any color
+// * Create at least two different vehicles in the main function and print their
+//   info
+//
+// Notes:
+// * Examples of car bodies can be Truck, Car, Scooter
+// * Examples of colors could be red, white, black
+// * It is not necessary to have data fields or function implementations
+//   for the vehicle bodies/colors
+
+trait Body {}
+trait Color {}
+
+#[derive(Debug)]
+struct Vehicle<B: Body, C: Color> {
+    body: B,
+    color: C
+}
+
+impl <B: Body, C: Color> Vehicle<B, C> {
+    fn new(body: B, color: C) -> Self {
+        Self{body, color}
+    }
+}
+
+#[derive(Debug)]
+struct Car{}
+impl Body for Car {}
+
+#[derive(Debug)]
+struct Truck{}
+impl Body for Truck {}
+
+#[derive(Debug)]
+struct Red{}
+impl Color for Red {}
+
+
+#[derive(Debug)]
+struct Blue{}
+impl Color for Blue {}
+
+fn main() {
+    let red_car = Vehicle::new(Car {}, Red {});
+    let blue_truck = Vehicle::new(Truck {}, Blue {});
+    println!("{:?}", red_car);
+    println!("{:?}", blue_truck);
+}
